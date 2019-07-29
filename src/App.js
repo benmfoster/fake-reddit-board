9df19bacb3329aec97839a98867c32e2d3d9c3ac
benmfoster <bfoster@marlboro.edu>
2019-07-29 15:54:13 -0400

    adds styles

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
         <div class="row">
 
           <div class="col-sm-8">
-            <h1 class="site-title"><router-link to="/">Fake Reddit</router-link><span>.</span></h1>
-            <p class="site-description">Misanthropes pile-driving the internet.</p>
+            <h1 class="site-title"><a href="/">Fake Reddit 📍 board</a><span>. exclusive</span></h1>
+            <p class="site-description">Members-only pillorying topics of social interest.</p>
           </div>
           {/* <!-- .col-sm-6 --> */}
 
@@ -42,12 +42,15 @@ function App() {
         {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
         <div class="collapse navbar-collapse" id="amalia-navbar-collapse">
           <ul class="nav navbar-nav">
+            <li><a href="https://sleepy-dawn-59018.herokuapp.com">Return to Fake Reddit</a></li>
+            <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Github <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="https://github.com/benmfoster/fake_reddit_vue">Frontend Repository (Vue.js)</a></li>
+                <li><a href="https://github.com/benmfoster/fake_reddit_board">Frontend Repository (React.js)</a></li>
                 <li><a href="https://github.com/benmfoster/fake_reddit">Backend Repository (Ruby on Rails)</a></li>
               </ul>
-              <li class="cta"><a href="https://benmfoster.github.io">About Me</a></li>
+            </li> 
+            <li class="cta"><a href="https://benmfoster.github.io">About Me</a></li>
           </ul>
           {/* <!-- .navbar-nav --> */}
         </div> {/* <!-- .navbar-collapse --> */}
@@ -61,7 +64,7 @@ function App() {
         <div class="row">
 
           <div class="col-md-4">
-            <h3>Fake Reddit Board<span>.</span></h3>
+            <h3>Fake Reddit<span>.</span></h3>
           </div>
           {/* <!-- .col-md-4 --> */}
 
