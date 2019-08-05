fb4eca91a1d8ac70a02d6477527da48d9da2c7f7
benmfoster <bfoster@marlboro.edu>
2019-08-05 12:53:37 -0400

    smoothes login to main content, makes posting dynamic

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ class App extends React.Component {
     super(props)
     const boardPosts = [];
     const user = {};
-    const newBoardPost = {};
+    const newBoardPosts = [];
     const email = '';
     const password = '';
     const boardPostText = '';
-    this.state = { email, password, boardPosts, user, newBoardPost, boardPostText };
+    const isLoggedIn = false
+    this.state = { email, password, boardPosts, user, newBoardPosts, boardPostText, isLoggedIn };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
@@ -31,10 +32,9 @@ handleSubmit(event) {
   }
   Axios.post('https://sleepy-dawn-59018.herokuapp.com/api/board_posts', params).then(response => {
     console.log("Success!", response.data);
-    this.setState({newBoardPost: response.data});
-  }).catch(error => {
-    this.errors = error.response.data.errors;
-    this.status = error.response.status;
+    const posts = this.state.newBoardPosts;
+    posts.unshift(response.data);
+    this.setState({newBoardPosts: posts});
   });
 }
 
@@ -44,18 +44,12 @@ handleLogin(event) {
     email: this.state.email,
     password: this.state.password
   };
-  Axios.post("http://localhost:3000/api/sessions", params).then(response => {
+  Axios.post("https://sleepy-dawn-59018.herokuapp.com/api/sessions", params).then(response => {
             Axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
-            localStorage.setItem("jwt", response.data.jwt);
-            localStorage.setItem("current_user_id", response.data.user_id);
-            if(response.data.user_id == true) {
-              console.log("success!")
-              return (
-                <button>Enter</button>
-              )
-            } else {
-              console.log('login invalid');
-            }
+            localStorage.setItem('jwt', response.data.jwt);
+            localStorage.setItem('current_user_id', response.data.user_id);
+            this.setState({isLoggedIn: true})
+            this.fetch();
           })
 }
 
@@ -79,29 +73,38 @@ deleteButton(post, i) {
   }
 }
 
-async componentDidMount() {
-  const [firstResponse, secondResponse] = await Promise.all([
-    Axios.get('https://sleepy-dawn-59018.herokuapp.com/api/board_posts'),
-    Axios.get('https://sleepy-dawn-59018.herokuapp.com/api/users/' + localStorage.getItem('current_user_id'))
-  ])
-  this.setState({
-    boardPosts: firstResponse.data.reverse(),
-    user: secondResponse.data,
-  });
-  console.log(this.state.boardPosts);
- }
+displayNewBoardPosts() {
+  if (this.state.newBoardPosts.length > 0) {
+    return (
+      this.state.newBoardPosts.map((boardPost, i) => (
+        <div class="col-md-12">
+          <article class="post">
+            <div class="news-container">
+            <span class="news-category">
+                <a href={'https://sleepy-dawn-59018.herokuapp.com/users/' + boardPost.user_id}>       
+                  {boardPost.authored_by} 
+                </a>
+            </span>
+              <span class="news-date">{boardPost.created_at}</span>
+                <div class="news-entry">
+                  <p>{boardPost.text}</p>
+                </div>
+            </div>
+            {this.deleteButton(boardPost, i)}            
+          </article>
+        </div>
+      ))
+    )
+  }
+}
 
-
-  render() {
-    if(!(localStorage.getItem('jwt'))) {
-      return (
-        <div>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-          Login to Fake Reddit Board
-        </button>
+login() {
+  return (
+    <div>
+        
         
       
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+       
           <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header">
@@ -112,7 +115,7 @@ async componentDidMount() {
               </div>
               <div class="modal-body">
               <div class="login">
-    <div class="container">
+    <div class="container col-xs-12">
       <form onSubmit={this.handleLogin}>
         <div class="form-group" onSubmit={this.handleLogin}>
           <label>Email:</label>
@@ -132,167 +135,192 @@ async componentDidMount() {
               </div>
             </div>
           </div>
+        
         </div>
-        </div>)
-    } else {
-       return (<div className="App">
-        <div id="app">
-      <div class="site-main">
+ 
+  )
+}
+
+logout() {
+  localStorage.removeItem("jwt");
+  localStorage.removeItem("current_user_id");
+}
+
+componentWillMount() {
+  if(localStorage.getItem('jwt')) {
+    this.setState({isLoggedIn: true,})
+    this.fetch();
+  }
+ }
+
+fetch() {
+  Axios.get('https://sleepy-dawn-59018.herokuapp.com/api/board_posts').then(response => {
+    this.setState({
+      boardPosts: response.data.reverse(),
+    });  
+ });
+  Axios.get('https://sleepy-dawn-59018.herokuapp.com/api/users/' + localStorage.getItem('current_user_id')).then(response => {
+    this.setState({
+      user: response.data,
+    });
+ });
+ }
+
+ mainPage() {
+   return (
+    <div className="App">
+    <div id="app">
+  <div class="site-main">
 
 
-      <div class="topbar">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-6">
-              <ul class="social-icon">
-                <li><a href="/">👑</a></li>
-              </ul>
-            </div>
-
-            <div class="col-sm-6">
-              <ul class="topbar-menu">
-                <li><a href="https://sleepy-dawn-59018.herokuapp.com">Return to Fake Reddit</a></li>
-              </ul>
-            </div>
-
-          </div>
+  <div class="topbar">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <ul class="social-icon">
+            <li><a href="/">👑</a></li>
+          </ul>
         </div>
+
+        <div class="col-sm-6">
+          <ul class="topbar-menu">
+            <li><a href="https://sleepy-dawn-59018.herokuapp.com">Return to Fake Reddit</a></li>
+          </ul>
+        </div>
+
       </div>
+    </div>
+  </div>
 
 
-        {/* <!-- header --> */}
-      <div class="header">
-        <div class="container">
-          <div class="row">
-  
-            <div class="col-sm-12">
-              <h1 class="site-title"><a href="/">Fake Reddit 📍 board</a><span>. exclusive </span> 🐋</h1>
-              <p class="site-description">Members-only pillorying topics of social interest.</p>
-            </div>
-            {/* <!-- .col-sm-6 --> */}
-  
-          </div>
-          {/* <!-- .row --> */}
+    {/* <!-- header --> */}
+  <div class="header">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-sm-12">
+          <h1 class="site-title"><a href="/">Fake Reddit 📍 board</a><span>. exclusive </span> 🐋</h1>
+          <p class="site-description">Members-only pillorying topics of social interest.</p>
         </div>
-        {/* <!-- .container --> */}
+        {/* <!-- .col-sm-6 --> */}
+
       </div>
-      {/* <!-- .header --> */}
-      {/* <!-- navbar --> */}
-      <nav class="navbar navbar-default">
-        <div class="container">
-          {/* <!-- Brand and toggle get grouped for better mobile display --> */}
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#amalia-navbar-collapse" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            {/* <!-- .navbar-toggle --> */}
-          </div>
-          {/* <!-- .navbar-header --> */}
-  
-          {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
-          <div class="collapse navbar-collapse" id="amalia-navbar-collapse">
-            <ul class="nav navbar-nav">
-              <li class="cta"><a href={'https://sleepy-dawn-59018.herokuapp.com/users/' + this.state.user.id}>My Profile</a></li>
-              <li class="cta"><a href="https://sleepy-dawn-59018.herokuapp.com/logout">Logout</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Github <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="https://github.com/benmfoster/fake_reddit_board">Frontend Repository (React.js)</a></li>
-                  <li><a href="https://github.com/benmfoster/fake_reddit">Backend Repository (Ruby on Rails)</a></li>
-                </ul>
-              </li> 
-              <li class="cta"><a href="https://benmfoster.github.io">About Me</a></li>
+      {/* <!-- .row --> */}
+    </div>
+    {/* <!-- .container --> */}
+  </div>
+  {/* <!-- .header --> */}
+  {/* <!-- navbar --> */}
+  <nav class="navbar navbar-default">
+    <div class="container">
+      {/* <!-- Brand and toggle get grouped for better mobile display --> */}
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#amalia-navbar-collapse" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        {/* <!-- .navbar-toggle --> */}
+      </div>
+      {/* <!-- .navbar-header --> */}
+
+      {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
+      <div class="collapse navbar-collapse" id="amalia-navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="cta"><a href={'https://sleepy-dawn-59018.herokuapp.com/users/' + this.state.user.id}>My Profile</a></li>
+          <li class="cta"><a href="https://sleepy-dawn-59018.herokuapp.com/logout" onClick={() => { this.logout() }}>Logout</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Github <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="https://github.com/benmfoster/fake_reddit_board">Frontend Repository (React.js)</a></li>
+              <li><a href="https://github.com/benmfoster/fake_reddit">Backend Repository (Ruby on Rails)</a></li>
             </ul>
-            {/* <!-- .navbar-nav --> */}
-          </div> {/* <!-- .navbar-collapse --> */}
-        </div>
-        {/* <!-- .container --> */}
-      </nav>
-      {/* <!-- .navbar --> */}
-     
-      <div class="post-entry">
-        <div class="container">
-          <div class="row">
+          </li> 
+          <li class="cta"><a href="https://benmfoster.github.io">About Me</a></li>
+        </ul>
+        {/* <!-- .navbar-nav --> */}
+      </div> {/* <!-- .navbar-collapse --> */}
+    </div>
+    {/* <!-- .container --> */}
+  </nav>
+  {/* <!-- .navbar --> */}
+ 
+  <div class="post-entry">
+    <div class="container">
+      <div class="row">
 
+        
+        <div class="col-md-8 col-md-offset-2">
+          <div class="row">
+          <form onSubmit={this.handleSubmit}>
+              <textarea type="text" class="input-lg form-control" rows="10" placeholder="Consume the internet. " name="boardPostText" value={this.state.boardPostText} onChange={this.handleChange} />
+              <div class="news-footer">
+            <input type="submit" value="Submit" class="btn btn-primary btn-lg" />
+            </div>
+          </form>
+
+              {this.displayNewBoardPosts()}
             
-            <div class="col-md-8 col-md-offset-2">
-              <div class="row">
-              <form onSubmit={this.handleSubmit}>
-                  <textarea type="text" class="input-lg form-control" rows="10" placeholder="Consume the internet. " name="boardPostText" value={this.state.boardPostText} onChange={this.handleChange} />
-                  <div class="news-footer">
-                <input type="submit" value="Submit" class="btn btn-primary btn-lg" />
-                </div>
-              </form>
-
-              
-                  <div class="col-md-12">
-                    <article class="post">
-                      <div class="news-container">
-                      <span class="news-category">
-                          <a href={'https://sleepy-dawn-59018.herokuapp.com/users/' + this.state.newBoardPost.user_id}>       
-                            {this.state.newBoardPost.authored_by} 
-                          </a>
-                      </span>
-                        <span class="news-date">{this.state.newBoardPost.created_at}</span>
-                          <div class="news-entry">
-                            <p>{this.state.newBoardPost.text}</p>
-                          </div>
+            {this.state.boardPosts.map((boardPost, i) => (
+              <div class="col-md-12">
+                <article class="post">
+                  <div class="news-container">
+                  <span class="news-category">
+                      <a href={'https://sleepy-dawn-59018.herokuapp.com/users/' + boardPost.user_id}>       
+                        {boardPost.authored_by} 
+                      </a>
+                  </span>
+                    <span class="news-date">{boardPost.created_at}</span>
+                      <div class="news-entry">
+                        <p>{boardPost.text}</p>
                       </div>
-                      {this.deleteButton(this.state.newBoardPost)}            
-                    </article>
                   </div>
-                
-                {this.state.boardPosts.map((boardPost, i) => (
-                  <div class="col-md-12">
-                    <article class="post">
-                      <div class="news-container">
-                      <span class="news-category">
-                          <a href={'https://sleepy-dawn-59018.herokuapp.com/users/' + boardPost.user_id}>       
-                            {boardPost.authored_by} 
-                          </a>
-                      </span>
-                        <span class="news-date">{boardPost.created_at}</span>
-                          <div class="news-entry">
-                            <p>{boardPost.text}</p>
-                          </div>
-                      </div>
-                      {this.deleteButton(boardPost, i)}            
-                    </article>
-                  </div>
-                ))}
-              
+                  {this.deleteButton(boardPost, i)}            
+                </article>
               </div>
-            </div>
-
+            ))}
+          
           </div>
         </div>
+
       </div>
-
     </div>
+  </div>
+
+</div>
 
 
-      <div class="footer">
-        <div class="container">
-          <div class="row">
-  
-            <div class="col-md-4">
-              <h3>Fake Reddit<span>.</span></h3>
-            </div>
-            {/* <!-- .col-md-4 --> */}
-  
-          </div>
-          {/* <!-- .row --> */}
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-4">
+          <h3>Fake Reddit<span>.</span></h3>
         </div>
-        {/* <!-- .container --> */}
+        {/* <!-- .col-md-4 --> */}
+
       </div>
-      {/* <!-- .footer --> */}
+      {/* <!-- .row --> */}
     </div>
-      </div>
-  )}
-      }
+    {/* <!-- .container --> */}
+  </div>
+  {/* <!-- .footer --> */}
+</div>
+  </div>
+   )
+ }
+
+
+  render() {
+    if(this.state.isLoggedIn) {
+      return this.mainPage();
+    } else {
+      return this.login();
     }
+        }
+
+        }
+    
 
 export default App;
